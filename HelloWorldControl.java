440be8918aa19e41ea3bc34786e3761fd33080ce
san963145 <2760759153@qq.com>
2016-08-03 04:25:26 -0400

    Update HelloWorldControl.java

--- HelloWorldControl.java
+++ HelloWorldControl.java
@@ -1,14 +1,28 @@
 package com.cn.myspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/test")
 public class HelloWorldControl {
 	
 	@RequestMapping("hello")
-	public String hello(){
+	public String hello(@RequestParam(value="id") int id){	
+		
+		System.out.println("id  :"+id);
 		return "hello";
+	}
+	
+	@RequestMapping(value="helloq",method={RequestMethod.POST})
+	public ModelAndView pass(@RequestParam("a") String a,@RequestParam("b") String b){		
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("message", "Hello World!");
+		mv.setViewName("result");
+		return mv;
 	}
 
 }
